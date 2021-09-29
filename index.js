e177b51191d9336b9132e072c6a4d47fb3fbdb5d
Will Brown <bro18085@byui.edu>
2021-09-29 21:27:54 +1000

    update prove02

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
-//const prove02 = require('./prove02.app')
+const prove02 = require('./prove02/app');
 
 app
     .use(express.static(path.join(__dirname, 'public')))
@@ -29,6 +29,7 @@ app
     .use('/ta02', ta02Routes)
     .use('/ta03', ta03Routes)
     .use('/ta04', ta04Routes)
+    .use('/prove02', prove02)
     .get('/', (req, res, next) => {
         // This is the primary index, always handled last.
         res.render('pages/index', {
